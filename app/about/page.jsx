6f1d1eabee277039581be2552ae3015447d48f23
nico <marcelonicolaspereyra@iresm.edu.ar>
2023-03-22 20:50:04 -0300

    primero

---
+++ app/about/page.jsx
@@ -0,0 +1,13 @@
+/** @format */
+
+"use client";
+import React from "react";
+import { Text, Button } from "@nextui-org/react";
+export default function about() {
+  return(
+    <div>
+        <Text>hola</Text>
+        <Button>hola</Button>
+    </div>
+  )
+}
---
+++ app/page.jsx
@@ -0,0 +1,12 @@
+'use client'
+
+import React from 'react'
+
+export default function index() {
+  return (
+    <div>
+      <h1> SDF </h1>
+      
+    </div>
+  )
+}
